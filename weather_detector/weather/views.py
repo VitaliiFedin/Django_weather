41cfb8013b5c8fc5e11f50876717499ea18d25ba
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-02-14 14:43:31 +0200

    interface update

--- weather_detector/weather/views.py
+++ weather_detector/weather/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+import json
+import urllib.request
+
 
 # Create your views here.
+def index(request):
+    if (request.method == 'POST'):
+        city = request.POST['city']
+        res = urllib.request.urlopen('https://api.openweathermap.org/data/2.5/weather?q='
+                                     + city + '&appid=1a86e99c48cca031cc32be6ce0883370').read()
+        json_data = json.loads(res)
+        data = {
+            'country_code': str(json_data['sys']['country']),
+            'coordinate': str(json_data['coord']['lat']) + ' ' +
+                              str(json_data['coord']['lon']),
+            'temp': str(round(int(json_data['main']['temp']) -273.15,0))+' C',
+            'feels_like': str(round(int(json_data['main']['feels_like']) - 273.15, 0)) + ' C',
+            'pressure': str(json_data['main']['pressure']),
+            'humidity': str(json_data['main']['humidity']),
+            'description': str(json_data['weather'] [0] ['description']),
+        }
+    else:
+        data = {}
+        city=''
+        redirect ('/')
+    return render(request, 'weather/index.html', {'city':city,'data':data})
